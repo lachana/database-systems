86ccf34e6857ad57ef7695a3777aafe6b3a93a20
lachana <dblab@dblab-VirtualBox>
2013-07-04 00:54:30 +0200

    New Dynamic Web Project FinalProject2610 created, classes airport, runway, shoppingService and tenant added

---
+++ FinalProject2610/src/airTraffic/Airport.java
@@ -0,0 +1,43 @@
+package airTraffic;
+
+public class Airport {
+	
+	private String name;
+	private String aviationCode;
+	private String location;
+	private String currency;
+	private String timeZone;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAviationCode() {
+		return aviationCode;
+	}
+	public void setAviationCode(String aviationCode) {
+		this.aviationCode = aviationCode;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getCurrency() {
+		return currency;
+	}
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+	public String getTimeZone() {
+		return timeZone;
+	}
+	public void setTimeZone(String timeZone) {
+		this.timeZone = timeZone;
+	}
+	
+
+}
---
+++ FinalProject2610/src/airTraffic/Tenant.java
@@ -0,0 +1,64 @@
+package airTraffic;
+
+import java.util.Date;
+
+public class Tenant {
+	
+	private int tId;
+	private boolean isActive;
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String email;
+	private String bankAccount;
+	private Date rentPaid;
+	
+	public int gettId() {
+		return tId;
+	}
+	public void settId(int tId) {
+		this.tId = tId;
+	}
+	public boolean isActive() {
+		return isActive;
+	}
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getBankAccount() {
+		return bankAccount;
+	}
+	public void setBankAccount(String bankAccount) {
+		this.bankAccount = bankAccount;
+	}
+	public Date getRentPaid() {
+		return rentPaid;
+	}
+	public void setRentPaid(Date rentPaid) {
+		this.rentPaid = rentPaid;
+	}
+}
