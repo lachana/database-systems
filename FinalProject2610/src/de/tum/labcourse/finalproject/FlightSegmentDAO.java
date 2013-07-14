f5e096b08980463bf7a6709274169f888caa8d9d
Anna Grebenkina <anna.grebenkina@gmail.com>
2013-07-14 21:41:37 +0200

    All flights information

---
+++ FinalProject2610/src/de/tum/labcourse/finalproject/FlightSegmentDAO.java
@@ -0,0 +1,109 @@
+package de.tum.labcourse.finalproject;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlightSegmentDAO extends AbstractDAO {
+
+	public List<FlightSegment> getAvailableFlights() throws SQLException {
+
+		List<FlightSegment> availableFlights = new ArrayList<FlightSegment>();
+
+		String query = new StringBuilder().append("SELECT * ")
+				.append("FROM flight_segment ").append("ORDER BY flight_number")
+				.toString();
+
+		Connection connection = getConnection();
+		PreparedStatement preparedStatement = connection
+				.prepareStatement(query);
+		ResultSet resultSet = preparedStatement.executeQuery();
+		while (resultSet.next()) {
+
+			FlightSegment flight = new FlightSegment();
+			flight.setFlight_number(resultSet.getString(1));
+			flight.setDate(resultSet.getDate(2));
+			flight.setGate_nr(resultSet.getString(3));
+			flight.setBoarding_time(resultSet.getDate(4));
+			flight.setMaerketing_carrier_flight_numbers(FlightSegmentDAO.split(resultSet.getString(5), ", "));
+			flight.setArriving_time(resultSet.getTimestamp(6));
+			flight.setDeparture_time(resultSet.getTimestamp(7));
+			flight.setArrival_airport(resultSet.getString(8));
+			flight.setDeparture_airport(resultSet.getString(9));
+			flight.setOperated_by(resultSet.getString(10));
+			flight.setFlightBy(resultSet.getInt(11));
+			availableFlights.add(flight);
+		}
+
+		resultSet.close();
+		return availableFlights;
+	}
+
+	public void addNewFlight(FlightSegment newFlight) throws SQLException {
+		List<FlightSegment> updatedFlights = new ArrayList<FlightSegment>();
+
+		String query = new StringBuilder()
+				.append("INSERT INTO flight_segment (flight_number, date, gate_nr, boarding_time,"
+						+ " marketing_carrier_flight_numbers, arriving_time, departure_time, arrival_airport, departure_airport,"
+						+ " operated_by, flightby) ")
+				.append("VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)").toString();
+
+		Connection connection = getConnection();
+		PreparedStatement pStmt = connection.prepareStatement(query);
+
+		pStmt.setString(1, newFlight.getFlight_number());
+		pStmt.setDate(2, new java.sql.Date(newFlight.getDate().getTime()));
+		pStmt.setString(3, newFlight.getGate_nr());
+		pStmt.setTimestamp(4, new Timestamp(newFlight.getBoarding_time().getTime()));
+		pStmt.setString(
+				5,
+				FlightSegmentDAO.join(
+						newFlight.getMaerketing_carrier_flight_numbers(), ", "));
+		pStmt.setTimestamp(6, newFlight.getArriving_time());
+		pStmt.setTimestamp(7, newFlight.getDeparture_time());
+		pStmt.setString(8, newFlight.getArrival_airport());
+		pStmt.setString(9, newFlight.getDeparture_airport());
+		pStmt.setString(10, (newFlight.getOperated_by()));
+		pStmt.setInt(11, newFlight.getFlightBy());
+		String qy = pStmt.toString();
+		pStmt.executeUpdate();
+	}
+
+	/**
+	 * Join array of strings to the string.
+	 * 
+	 * @param r
+	 *            Array of strings
+	 * @param d
+	 *            delimiter
+	 * @return String
+	 */
+	public static String join(String r[], String d) {
+		if (r.length == 0)
+			return "";
+		StringBuilder sb = new StringBuilder();
+		int i;
+		for (i = 0; i < r.length - 1; i++)
+			sb.append(r[i] + d);
+		return sb.toString() + r[i];
+	}
+	/**
+	 * Split string by delimiter with regard to the empty value and single element.
+	 * 
+	 * @param s String to split
+	 * @param d Delimiter
+	 * @return Array of strings
+	 */
+	public static String[] split(String s, String d) {
+		if(s == null)
+			return new String[0];
+		else if(s.lastIndexOf(",") < 0)
+			return new String[] {s};
+		else
+			return s.split(", ");
+	}
+}
---
+++ FinalProject2610/src/de/tum/labcourse/finalproject/FlightSegmentServlet.java
@@ -0,0 +1,108 @@
+package de.tum.labcourse.finalproject;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FlightSegmentServlet
+ */
+@WebServlet(name = "FlightSegmenServlet", description = "Existing flights + new flight", urlPatterns = { "/FlightSegmenServlet" })
+public class FlightSegmentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FlightSegmentServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		FlightSegmentDAO dao = new FlightSegmentDAO();
+		//Get all flights
+		List<FlightSegment> allFlights;
+		try {
+			allFlights = dao.getAvailableFlights();
+		} catch (SQLException e) {
+			// Throw 500 ERROR if something goes wrong
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			return;
+		}
+		//Add them as attributes to the request to make available in the JSP view
+		request.setAttribute("allFlights", allFlights);
+		//Set view
+		RequestDispatcher view = request.getRequestDispatcher("flights.jsp");
+		view.forward(request, response); 
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	    DateFormat timeFormat = new SimpleDateFormat("hh:mm:ss");
+	    DateFormat timestampFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+		FlightSegmentDAO flights = new FlightSegmentDAO();
+		FlightSegment flight = new FlightSegment();
+		flight.setFlight_number(request.getParameter("flightNumber"));
+		try {
+			flight.setDate(dateFormat.parse(request.getParameter("flightDate")));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		flight.setGate_nr(request.getParameter("gateNr"));
+
+		try {
+			flight.setBoarding_time(timeFormat.parse(request.getParameter("boardingTime")));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		flight.setMaerketing_carrier_flight_numbers(request.getParameter("marketingFlightsNumber").split(", "));
+		try {
+			flight.setArriving_time(new Timestamp (timestampFormat.parse(request.getParameter("arrivingTime")).getTime()));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			flight.setDeparture_time(new Timestamp (timestampFormat.parse(request.getParameter("departureTime")).getTime()));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		flight.setArrival_airport(request.getParameter("arrivalAirport"));
+		flight.setDeparture_airport(request.getParameter("departureAirport"));
+		flight.setOperated_by(request.getParameter("operatedBy"));
+		flight.setFlightBy(Integer.parseInt(request.getParameter("flightBy")));
+		
+		try {
+			flights.addNewFlight(flight);
+		} catch (SQLException e) {
+			// Throw 500 ERROR if something goes wrong
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			return;
+		}
+        response.sendRedirect("/AirTrafficProject/flights");
+	}
+}
